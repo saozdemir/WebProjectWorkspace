0148c9186463d0acd3ec9cc399d8f2ceea1e138f
saozdemir <43U2422a**>
2024-10-15 15:29:09 +0300

    first commit

---
+++ component-structure/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Login from './Login'
+
+// Login komponentinde const olarak export edilen alan burada import edildi.
+import { users } from './Login'
+
+
+function App() {
+  console.log(users);
+  return (
+    //Fragment
+    <>
+      <Login />
+    </>
+  )
+}
+
+export default App
