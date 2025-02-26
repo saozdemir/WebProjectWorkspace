714a65974f85f7eba3317df980932aca604df441
saozdemir <43U2422a**>
2025-02-26 16:42:59 +0300

    react-extra app

--- react/setnodevars.bat
+++ react/setnodevars.bat
@@ -1,8 +1,12 @@
+@REM NodeJS LTS v22.13.1
 @echo off
 set PATH=C:\Users\xsaozdemir\Program Files\NodeJS\node-v22.13.1\;C:\Users\xsaozdemir\Program Files\NodeJS\node-v22.13.1\node_modules\npm;C:\Users\xsaozdemir\Program Files\NodeJS\node-v22.13.1\node_modules\.bin;%PATH%
 set NODE_PATH=C:\Users\xsaozdemir\Program Files\NodeJS\node-v22.13.1\node_modules
 
+@REM NodeJS LTS v22.14.0
+set PATH=C:\Users\xsaozdemir\Program Files\NodeJS\node-v22.14.0\;C:\Users\xsaozdemir\Program Files\NodeJS\node-v22.14.0\node_modules\npm;C:\Users\xsaozdemir\Program Files\NodeJS\node-v22.14.0\node_modules\.bin;%PATH%
+set NODE_PATH=C:\Users\xsaozdemir\Program Files\NodeJS\node-v22.14.0\node_modules
 
-PS Kullanarak
-$PATH = [Environment]::GetEnvironmentVariable("PATH", "Machine") + ";C:\Users\xsaozdemir\Program Files\NodeJS\node-v22.13.1\;C:\Users\xsaozdemir\Program Files\NodeJS\node-v22.13.1\node_modules\npm;C:\Users\xsaozdemir\Program Files\NodeJS\node-v22.13.1\node_modules\.bin"
-[Environment]::SetEnvironmentVariable("PATH", $PATH, "Machine")
+@REM PS Kullanarak
+@REM $PATH = [Environment]::GetEnvironmentVariable("PATH", "Machine") + ";C:\Users\xsaozdemir\Program Files\NodeJS\node-v22.13.1\;C:\Users\xsaozdemir\Program Files\NodeJS\node-v22.13.1\node_modules\npm;C:\Users\xsaozdemir\Program Files\NodeJS\node-v22.13.1\node_modules\.bin"
+@REM [Environment]::SetEnvironmentVariable("PATH", $PATH, "Machine")
